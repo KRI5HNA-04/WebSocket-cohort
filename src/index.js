0531dff89239ae716ce851bd24144ce9781cc720
Krishna <krishna61223@gmail.com>
2025-08-02 23:31:50 +0530

    Initial update

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { WebSocketServer } from "ws";
+
+const app = express();
+const httpServer = app.listen(8080);
+
+const wss = new WebSocketServer({ server: httpServer });
+
+wss.on("connection", function connection(socket) {
+  let userCount = 0;
+  socket.on("error", console.error);
+
+  socket.on("message", function message(data, isBinary) {
+    console.log("received: %s", data);
+
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+
+  console.log("user connected ", ++userCount);
+
+  socket.send("user connected ", userCount);
+  socket.send("Hello! Message From Server!!");
+});
