a5b4a1b407673cf85a6ba7fa4adb296fce1b818c
Krishna <krishna61223@gmail.com>
2025-08-03 22:00:23 +0530

    discard index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
-import express from "express";
-import { WebSocketServer } from "ws";
+// import express from "express";
+// import { WebSocketServer } from "ws";
 
-const app = express();
-const httpServer = app.listen(8080);
+// const app = express();
+// const httpServer = app.listen(8080);
 
-const wss = new WebSocketServer({ server: httpServer });
+// const wss = new WebSocketServer({ server: httpServer });
 
-wss.on("connection", function connection(socket) {
-  let userCount = 0;
-  socket.on("error", console.error);
+// wss.on("connection", function connection(socket) {
+//   let userCount = 0;
+//   socket.on("error", console.error);
 
-  socket.on("message", function message(data, isBinary) {
-    console.log("received: %s", data);
+//   socket.on("message", function message(data, isBinary) {
+//     console.log("received: %s", data);
 
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
-  });
+//     wss.clients.forEach(function each(client) {
+//       if (client.readyState === WebSocket.OPEN) {
+//         client.send(data, { binary: isBinary });
+//       }
+//     });
+//   });
 
-  console.log("user connected ", ++userCount);
+//   console.log("user connected ", ++userCount);
 
-  socket.send("user connected ", userCount);
-  socket.send("Hello! Message From Server!!");
-});
+//   socket.send("user connected ", userCount);
+//   socket.send("Hello! Message From Server!!");
+// });
+
+// this is also one of the ways to create a websocket server
